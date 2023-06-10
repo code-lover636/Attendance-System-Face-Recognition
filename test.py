a2d04d5903edb6479cd3837e9a5de9b69c767521
Aravind Ashokan <aravindashokan636@gmail.com>
2023-06-10 11:53:57 +0530

    Update test

--- test.py
+++ test.py
@@ -7,6 +7,9 @@ import numpy as np
 import cv2
 import pickle
 import os
+import csv
+import time
+from datetime import datetime
 
 video = cv2.VideoCapture(0)
 facedetect = cv2.CascadeClassifier('data/hfd.xml')
@@ -19,6 +22,8 @@ with open('data/faces.pkl', 'rb') as f:
 knn = KNeighborsClassifier(n_neighbors=5)
 knn.fit(FACES, LABELS)
 
+
+
 while True:
     ret, frame = video.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -27,10 +32,30 @@ while True:
         crop_img = frame[y:y+h, x:x+w : ]
         resized_img = cv2.resize(crop_img, (50,50)).flatten().reshape(1, -1)
         output  = knn.predict(resized_img)
+        
+        ts = time.time()
+        date = datetime.fronttimestamp(ts).strftime("%d-%m-%Y")
+        timestamp = datetime.frontimestamp(ts).strftime("%H:%M:%S")
+        exist = os.path.isfile("Attendance/Attendace"+date+".csv")
         cv2.putText(frame, str(output[0]), (x,y-15), cv2.FONT_HERSHEY_COMPLEX, 1, (255,255,255), 1)
         cv2.rectangle(frame, (x,y), (x+w, y+h), (50,50,255), 1)
         
+        attendance = [str(output[0], str(timestamp))]
+        
     cv2.imshow("Frame", frame)
+    if cv2.waitKey(1) == ord('o'):
+        if exist:
+            with open("Attendance/Attendance"+date+".csv", "+a") as cf:
+                writer = csv.writer(cf)
+                writer.writerow(["Name", "Time"])
+                writer.writerow(attendance)
+        else:
+            with open("Attendance/Attendance"+date+".csv", "+a") as cf:
+                writer = csv.writer(cf)
+                writer.writerow(["Name", "Time"])
+                writer.writerow(attendance)
+                
+                
     if cv2.waitKey(1) == ord('q'):
         break
     
